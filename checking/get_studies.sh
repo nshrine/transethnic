117c4a3907296f1a11eb571aee9e52745b0acb1c
Nick Shrine <nick.shrine@leicester.ac.uk>
2020-10-21 10:48:59 +0100

    Tidy initial commit

--- checking/get_studies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASE=/scratch/gen1/agib1/trans-ethnic/raw/big_METAL
-PHENO=FF
-SNP=rs13141641
-
-grep "Input File" ${BASE}/${PHENO}_big_METAL_results1.tbl.info | cut -f 8 -d '/' > ${PHENO}.studies
