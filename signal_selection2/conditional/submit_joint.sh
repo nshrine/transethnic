c2d01a6ccf099a45ec1415b95848da2d26190d03
Nick Shrine <nick.shrine@leicester.ac.uk>
2020-11-12 13:34:28 +0000

    run joint

--- signal_selection2/conditional/submit_joint.sh
+++ signal_selection2/conditional/submit_joint.sh
@@ -12,12 +12,7 @@ n=$((`cat ${LOCI} | wc -l` - 1))
 
 for i in `seq 1 $n`; do
     LOCUS=locus$i
-    r=1
-
-    while [ -s ${PHENO}_chr${CHR}_${LOCUS}_round${r}.cond ] && [ `cat ${PHENO}_chr${CHR}_${LOCUS}_round${r}.cond | wc -l` -eq $r ]; do
-        ROUND=$r
-        let r=$((r + 1))
-    done
+    ROUND=`wc -l ${PHENO}_chr${CHR}_${LOCUS}_round*.cond | head -n -1 | awk '{ print $1 }' | sort | uniq -d`
 
     if [ $ROUND -gt 1 ]; then
         echo -n "Running ${PHENO} chr${CHR} ${LOCUS}, ${ROUND} signals: "
